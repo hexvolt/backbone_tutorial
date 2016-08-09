c338da9b46ea0cc9112096826fe2a8c60a77a0b2
hexvolt <hexvolt@gmail.com>
2016-08-09 23:36:41 +0300

    basic template get used

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,16 @@
 var AppView = Backbone.View.extend({
 
     el: '#container',
+    template: _.template("<h3>Hello, <%= user %></h3>"),
 
     initialize: function(){
         this.render();
     },
 
     render: function(){
-        this.$el.html("Hello World");
+        this.$el.html(
+            this.template({user: 'anonymous'})
+        );
     }
 });
 
