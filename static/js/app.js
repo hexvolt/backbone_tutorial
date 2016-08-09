b3708812be8dc4aeee352f9ce47ed5d9ee89cf0a
hexvolt <hexvolt@gmail.com>
2016-08-09 23:41:36 +0300

    view expanded with comments

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,10 @@
 var AppView = Backbone.View.extend({
+    // This is a basic view which contains:
+    // - `el` (html element this view is bound to)
+    // - template (will be rendered to the el)
+    // - initialize (a method that will be called when a view get instantiated)
+    // - render (a method that renders template with the data and
+    //           inserts it into html)
 
     el: '#container',
     template: _.template("<h3>Hello, <%= user %></h3>"),
@@ -14,4 +20,5 @@ var AppView = Backbone.View.extend({
     }
 });
 
+// instantiating view
 var appView = new AppView();
