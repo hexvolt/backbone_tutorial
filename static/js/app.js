7460328f1410130d7042e720d249b0a4395949b4
hexvolt <hexvolt@gmail.com>
2016-08-14 12:42:20 +0300

    AppView implemented

--- static/js/app.js
+++ static/js/app.js
@@ -23,7 +23,7 @@ var ExampleView = Backbone.View.extend({
 var app = {};
 
 // Model
-app.Todo = Backbone.Model.extend({
+app.TodoModel = Backbone.Model.extend({
     // Note that it is a class that represents the data model, not an instance
     defaults: {
         title: '',
@@ -32,18 +32,18 @@ app.Todo = Backbone.Model.extend({
 });
 
 // Collection
-app.TodoList = Backbone.Collection.extend({
-    model: app.Todo,
+app.TodoListCollection = Backbone.Collection.extend({
+    model: app.TodoModel,
     localStorage: new Store("backbone-todo")
     // normally here should be specified an URL of the backend data storage
 });
-app.todoList = new app.TodoList();  // global instance of the collection
+app.collectionTodoList = new app.TodoListCollection();  // global instance of the collection
 
 
 // View
 app.TodoView = Backbone.View.extend({
     tagName: 'li',      // will be wrapped into <li>
-    template: _.template($('#item-template').html()),
+    template: _.template(this.$('#item-template').html()),
     render: function(){
         this.$el.html(this.template(this.model.toJSON()));
         return this;
@@ -55,28 +55,46 @@ app.AppView = Backbone.View.extend({
     el: '#todoapp',
 
     initialize: function() {
-
+        this.elTodoList = this.$('#todo-list');
+        this.input = this.$('#new-todo');
+        // finally bind trigger in order to keep page updated with the models
+        app.collectionTodoList.on('add', this.addOne, this);
+        app.collectionTodoList.on('reset', this.addAll, this);
+        // the last step - fetch all the existing data from the storage
+        app.collectionTodoList.fetch();
     },
 
     events: {
-
+        'keypress #new-todo': 'createTodoOnEnter'
     },
+
     // methods
+    createTodoOnEnter: function(e) {
+        // append a new model instance to the collection
+        if ( e.which !== 13 || !this.input.val().trim() )
+            return;
 
-    createTodoOnEnter: function() {
-
+        app.collectionTodoList.create(this.getCleanedAttributes());
+        this.input.val('');
     },
 
-    addOne: function() {
-
+    addOne: function(modelTodo) {
+        // render and add an existing model instance to the list
+        var view = new app.TodoView({model: modelTodo});
+        this.elTodoList.append(view.render().el);
     },
 
     addAll: function() {
-
+        // re-render an entire list based on the current collection content
+        this.elTodoList.html('');
+        app.collectionTodoList.each(this.addOne, this);
     },
 
-    getNewAttributes: function() {
-
+    getCleanedAttributes: function() {
+        return {
+          title: this.input.val().trim(),
+          completed: false
+        }
     }
 });
 
